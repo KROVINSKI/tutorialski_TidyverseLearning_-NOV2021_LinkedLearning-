fd8e55cbf765e8850517a49b6979a3f0110dfa6c
KROVINSKI <51680594+KROVINSKI@users.noreply.github.com>
2021-10-22 15:33:47 -0700

    df and tibbles, indexes and the tibbles
    the trouble with tibbles

---
+++ R_(allScriptFiles)/TibblesOverview.R
@@ -0,0 +1,15 @@
+# What are data.frames? 
+# what are tibbles? 
+
+# data.frames is base R's standard rectangular data store. 
+# Tibble is the tidyverse's "standard rectangular data store".
+
+# not part of the tidverse
+#Data.table is highly optimized "rectangular data store"
+# (data.table from it's own package)
+# These data tables are not part of the tidyverse
+
+# Matrices are different and not designed for "arbitrary rectangular data"
+
+# Tibbles are in the Tidyverse
+
---
+++ R_(allScriptFiles)/index_and_tibbles.R
@@ -0,0 +1,16 @@
+# want the first column
+baseR_dataframe[,1]
+# can see it as a factor
+
+baseR_dataframe[[1]]
+# same
+
+
+tidyverse_tibble[,1]
+tidyverse_tibble[[1]]
+#returns column as a vector
+
+tidyverse_tibble[[2]]
+# the second index of this object
+
+# always use double square brackets to always get what you mean to call
